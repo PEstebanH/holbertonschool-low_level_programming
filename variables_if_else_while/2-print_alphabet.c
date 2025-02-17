618f0c7043970ec26b0b431e8cc9261505aace62
root <root@Sora-Portable>
2025-02-17 14:20:54 +0100

    add file 3

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+    i = 97;
+    while (i < 123)
+    {
+        putchar(i);
+        i++;
+    }
+	return (0);
+}
