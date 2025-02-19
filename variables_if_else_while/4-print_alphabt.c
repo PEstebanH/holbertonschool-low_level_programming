ae253fe59d09c78178e8a32e661283f87ea6d99d
root <root@Sora-Portable>
2025-02-19 14:10:15 +0100

    test

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,18 +9,18 @@
  */
 int main(void)
 {
-    int a;
+	int a;
 
-    a = 97;
+	a = 97;
 
-    while (a <= 122)
-    {
-        if (a != 101 && a != 113)
-        {
-            putchar(a);
-        }
-        a++;
-    }
-    putchar('\n');
-    return (0);
+	while (a <= 122)
+	{
+		if (a != 101 && a != 113)
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
 }
