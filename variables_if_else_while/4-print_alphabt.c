cabf08cd1958f2978a70712fec2a0700587854dc
root <root@Sora-Portable>
2025-02-19 14:08:48 +0100

    test

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int a;
+
+    a = 97;
+
+    while (a <= 122)
+    {
+        if (a != 101 && a != 113)
+        {
+            putchar(a);
+        }
+        a++;
+    }
+    putchar('\n');
+    return (0);
+}
