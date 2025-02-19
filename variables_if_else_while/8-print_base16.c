fe2daba56ab6253235df6dc6c2ce3c88c1a07064
root <root@Sora-Portable>
2025-02-19 14:40:00 +0100

    Add FIle hex

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+    int i;
+    int c;
+    
+    i = 48;
+    c = 97;
+    while (i <= 57)
+    {
+        putchar(i);
+        i++;
+    }
+    while (c <= 102)
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+    return (0);
+}
