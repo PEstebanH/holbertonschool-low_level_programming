16e153b163fb5943ecb3192ad3da4932fd38677a
root <root@Sora-Portable>
2025-02-21 01:34:27 +0100

    test

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,21 +1,34 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 void jack_bauer(void)
 {
-    int minutes, hours;
+    int h1, h2, m1, m2;
 
-    for (minutes = 0; minutes < 1440; minutes++)
+    for (h1 = 0; h1 <= 2; h1++)
     {
-        hours = minutes / 60;
-        minutes = minutes % 60;
-        _putchar((hours / 10) + '0');
-        _putchar((hours % 10) + '0');
-        _putchar(':');
-        _putchar((minutes / 10) + '0');
-        _putchar((minutes % 10) + '0');
-        _putchar('\n');
+        for (h2 = 0; h2 <= 9; h2++)
+        {
+            for (m1 = 0; m1 <= 5; m1++)
+            {
+                for (m2 = 0; m2 <= 9; m2++)
+                {
+                    if (h1 == 2 && h2 > 3)
+                    {
+                        break;
+                    }
+                    _putchar(h1 + '0');
+                    _putchar(h2 + '0');
+                    _putchar(':');
+                    _putchar(m1 + '0');
+                    _putchar(m2 + '0');
+                    _putchar('\n');
+                }
+            }
+        }
     }
 }
