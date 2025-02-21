cd8fa2828baffd90c1da8c76c6a8568a8987821a
root <root@Sora-Portable>
2025-02-21 23:21:14 +0100

    test

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_to_98 - Entry point
+ * @i: the number to check
+ * Return: void
  */
 
- void print_to_98(int i)
+void print_to_98(int i)
 {
-    if (i < 98)
-    {
-        for (; i < 98; i++)
-            printf("%d, ", i);
-    }
-    else if (i > 98)
-    {
-        for (; i > 98; i--)
-            printf("%d, ", i);
-    }
-    if (i == 98)
-        printf("%d\n", i);
+	if (i < 98)
+	{
+		for (; i < 98; i++)
+			printf("%d, ", i);
+	}
+	else if (i > 98)
+	{
+		for (; i > 98; i--)
+			printf("%d, ", i);
+	}
+	if (i == 98)
+		printf("%d\n", i);
 }
