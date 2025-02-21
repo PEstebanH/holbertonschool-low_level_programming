709242d76ec1daf5f264022dc211a711b8813bcb
root <root@Sora-Portable>
2025-02-21 01:37:36 +0100

    test

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(void)
+{
+    int i;
+
+    for (i = 0; i < 98; i++)
+    {
+        printf("%d, ", i);
+    }
+    printf("98\n");
+}
