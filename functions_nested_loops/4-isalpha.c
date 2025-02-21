c53f53c5e3df5849b7b81d434c6f9e9a92e5d628
root <root@Sora-Portable>
2025-02-21 01:22:45 +0100

    test

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ * 
+ */
+int isalpha(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return (1);
+    }
+    return (0);
+}
