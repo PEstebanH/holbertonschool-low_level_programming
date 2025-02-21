f2bc6076379ded521a436aca8c5fa9cbbb1f03af
root <root@Sora-Portable>
2025-02-21 23:15:39 +0100

    test

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * jack_bauer - Entry point
+ *  * Return: void
  */
 void jack_bauer(void)
 {
-    int h1, h2, m1, m2;
+	int h1, h2, m1, m2;
 
-    for (h1 = 0; h1 <= 2; h1++)
-    {
-        for (h2 = 0; h2 <= 9; h2++)
-        {
-            for (m1 = 0; m1 <= 5; m1++)
-            {
-                for (m2 = 0; m2 <= 9; m2++)
-                {
-                    if (h1 == 2 && h2 > 3)
-                    {
-                        break;
-                    }
-                    _putchar(h1 + '0');
-                    _putchar(h2 + '0');
-                    _putchar(':');
-                    _putchar(m1 + '0');
-                    _putchar(m2 + '0');
-                    _putchar('\n');
-                }
-            }
-        }
-    }
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 9; h2++)
+		{
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					if (h1 == 2 && h2 > 3)
+					{
+						break;
+					}
+					_putchar(h1 + '0');
+					_putchar(h2 + '0');
+					_putchar(':');
+					_putchar(m1 + '0');
+					_putchar(m2 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
