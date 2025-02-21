3a06c6f83055159f6344e233b7629f37fb3ecca6
root <root@Sora-Portable>
2025-02-21 01:10:04 +0100

    test

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char l;
+	int i = 0;
+	while (i < 10)
+	{
+		 
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+		i++;
+	}
+	_putchar('\n');
+}
+
