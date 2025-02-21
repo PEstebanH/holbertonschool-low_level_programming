deac93e6b4993363e75768a5ad97050211156b1b
root <root@Sora-Portable>
2025-02-21 01:35:20 +0100

    test

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+    int i, j, n;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            n = i * j;
+            if (j == 0)
+            {
+                _putchar(n + '0');
+            }
+            else if (n <= 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+                _putchar(n + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar((n / 10) + '0');
+                _putchar((n % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
+}
