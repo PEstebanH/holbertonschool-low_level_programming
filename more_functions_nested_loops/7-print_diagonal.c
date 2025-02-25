c379b35fdbf7248190026bfae1f397a15e7b7f9d
root <root@Sora-Portable>
2025-02-25 11:41:24 +0100

    Add File

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: length of the line
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int p, j;
+
+	for (p = 0; p < n; p++)
+	{
+		for (j = 0; j < p; j++);:
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
