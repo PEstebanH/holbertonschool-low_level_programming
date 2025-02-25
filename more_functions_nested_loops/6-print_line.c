a7e2117c436b27cfbf013fb6e7a444ed603a1efb
root <root@Sora-Portable>
2025-02-25 11:36:25 +0100

    Add File+betty

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: length of the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int p;
+
+	for (p = 0; i < n; p++)
+		_putchar('_');
+
+	_putchar('\n');
+}
