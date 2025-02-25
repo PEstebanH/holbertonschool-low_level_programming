3a7f007e6c844fd1d63d6c431de09147994073dc
root <root@Sora-Portable>
2025-02-25 15:15:41 +0100

    Add File+Betty

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,16 @@
 
 void print_triangle(int size)
 {
-    int n, j, k;
+	int n, j, k;
 
-    if (size <= 0)
-        _putchar('\n');
-    for (n = 1; n <= size; n++)
-    {
-        for (j = size - n; j > 0; j--)
-            _putchar(' ');
-        for (k = 0; k < n; k++)
-            _putchar('#');
-        _putchar('\n');
-    }
+	if (size <= 0)
+		_putchar('\n');
+	for (n = 1; n <= size; n++)
+	{
+		for (j = size - n; j > 0; j--)
+			_putchar(' ');
+		for (k = 0; k < n; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
