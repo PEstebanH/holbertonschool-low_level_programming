d9f21d0c2f598b6f6fb7b487291bf465c5229939
root <root@Sora-Portable>
2025-02-25 15:14:09 +0100

    test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
-
 /**
- * fizz_buzz - prints the numbers from 1 to 100, followed by a new line
- * For multiples of three, print Fizz instead of the number
- * For the multiples of five, print Buzz
- * For numbers which are multiples of both three and five, print FizzBuzz
+ * main - entry point
+ *
+ * Description: prints FizzBuzz
  *
  * Return: void
  */
-void fizz_buzz(void)
+int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 1; i <= 100; i++)
+	for (n = 1; n <= 100; n++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
-		if (i % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+		else if (n % 5 == 0)
 			printf("Buzz");
-		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d", i);
-		if (i < 100)
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", n);
+
+		if (n < 100)
 			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("\n");
+	return (0);
 }
