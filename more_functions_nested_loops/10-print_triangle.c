943d2dbefa69f530f1f7c1279f86a94388008ac2
root <root@Sora-Portable>
2025-02-25 15:15:16 +0100

    test

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+    int n, j, k;
+
+    if (size <= 0)
+        _putchar('\n');
+    for (n = 1; n <= size; n++)
+    {
+        for (j = size - n; j > 0; j--)
+            _putchar(' ');
+        for (k = 0; k < n; k++)
+            _putchar('#');
+        _putchar('\n');
+    }
+}
